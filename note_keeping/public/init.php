b748eb019e0dec31a2aea87b8a329cf56ca472c6
Walter Cumbucane <cumbucane.walter@gmail.com>
2024-06-04 18:48:42 +0200

    Solution ADDED

--- note_keeping/public/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(0);
-
-require_once('config.php');
-require_once('lib.php');
-
-session_save_path(TEMP_DIR);
-session_start();
--- note_keeping/public/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('init.php');
-
-$_SESSION = array();
-session_destroy();
-
-redirect('/?page=home');
